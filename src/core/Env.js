dd23ed8369a4bc51a14a94c68ec9f5e26860c228
yinm <yinm.cminor9@gmail.com>
2018-05-02 20:03:33 +0900

    Add properties of Env class

---
+++ src/core/Env.js
@@ -0,0 +1,34 @@
+getJasmineRequireObj().Env = function(j$) {
+  /**
+   * _Note:_ Do not construct this directly, Jasmine will make one during booting.
+   * @name Env
+   * @classdesc The Jasmine environment
+   * @constructor
+   */
+  function Env(options) {
+    options = options || {}
+
+    const self = this
+    const global = options.global || j$.getGlobal()
+
+    let totalSpecsDefined = 0
+
+    const realSetTimeout = global.setTimeout
+    const realClearTimeout = global.clearTimeout
+    const clearStack = j$.getClearStack(global)
+    this.clock = new j$.Clock(global, function() { return new j$.DelayedFunctionScheduler() }, new j$.MockDate(global))
+
+    let runnableResources = {}
+
+    let currentSpec = null
+    let currentlyExecutingSuites = []
+    let currentDeclarationSuite = null
+    let throwOnExpectationFailure = false
+    let stopOnSpecFailure = false
+    let random = true
+    let seed = null
+    let hasFailure = false
+  }
+
+  return Env
+}

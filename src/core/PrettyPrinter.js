7ea0a7f80059ae7d1f6e8e5b1f5191b63a60847e
yinm <yinm.cminor9@gmail.com>
2018-05-03 13:17:42 +0900

    Add hasCustomToStirng

--- src/core/PrettyPrinter.js
+++ src/core/PrettyPrinter.js
@@ -6,6 +6,13 @@ getJasmineRequireObj().pp = function(j$) {
     this.stringParts = []
   }
 
+  function hasCustomToStirng(value) {
+    // value.toString !== Object.prototype.toString if value has no custom toString but is from another context (e.g.
+    // iframe, web worker)
+    return j$.isFunction_(value.toString)
+      && value.toString !== Object.prototype.toString
+      && (value.toString() !== Object.prototype.toString.call(value))
+  }
 
 
   PrettyPrinter.prototype.emitScalar = function(value) {

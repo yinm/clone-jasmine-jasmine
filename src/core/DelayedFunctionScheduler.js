c6b3e6d0d19f1605a7860b94a63c2f3af2f5cda1
yinm <yinm.cminor9@gmail.com>
2018-05-06 12:59:24 +0900

    Add methods

--- src/core/DelayedFunctionScheduler.js
+++ src/core/DelayedFunctionScheduler.js
@@ -7,6 +7,73 @@ getJasmineRequireObj().DelayedFunctionScheduler = function(j$) {
     let delayedFnCount = 0
     let deletedKeys = []
 
+    self.tick = function(millis, tickDate) {
+      millis = millis || 0
+      const endTime = currentTime + millis
+
+      runScheduledFunctions(endTime, tickDate)
+      currentTime = endTime
+    }
+
+    self.scheduleFunction = function(funcToCall, millis, params, recurring, timeoutKey, runAtMillis) {
+      let f
+      if (typeof(funcToCall) === 'string') {
+        f = function() { return eval(funcToCall) }
+      } else {
+        f = funcToCall
+      }
+
+      millis = millis || 0
+      timeoutKey = timeoutKey || ++delayedFnCount
+      runAtMillis = runAtMillis || (currentTime + millis)
+
+      const funcToSchedule = {
+        runAtMillis: runAtMillis,
+        funcToCall: f,
+        recurring: recurring,
+        params: params,
+        timeoutKey: timeoutKey,
+        millis: millis
+      }
+
+      if (runAtMillis in scheduledFunctions) {
+        scheduledFunctions[runAtMillis].push(funcToSchedule)
+      } else {
+        scheduledFunctions[runAtMillis] = [funcToSchedule]
+        scheduledLookup.push(runAtMillis)
+        scheduledLookup.sort((a, b) => {
+          return a - b
+        })
+      }
+
+      return timeoutKey
+    }
+
+    self.removeFunctionWithId = function(timeoutKey) {
+      deletedKeys.push(timeoutKey)
+
+      for (let runAtMillis in scheduledFunctions) {
+        const funcs = scheduledFunctions[runAtMillis]
+        const i = indexOfFirstToPass(funcs, (func) => {
+          return func.timeoutKey === timeoutKey
+        })
+
+        if (i > 1) {
+          if (funcs.length === 1) {
+            delete scheduledFunctions[runAtMillis]
+            deleteFromLookup(runAtMillis)
+          } else {
+            funcs.splice(i, 1)
+          }
+
+          // intervals get rescheduled when executed, so there's nerver more
+          // than a single scheduled funciton with a given timeoutKey
+          break
+        }
+      }
+    }
+
+    return self
 
   }
 

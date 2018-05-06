e5ac83e0cda140f7c35a48eb1a90bbad5530d1e1
yinm <yinm.cminor9@gmail.com>
2018-05-06 12:51:28 +0900

    Add DelayedFunctionScheduler

---
+++ src/core/DelayedFunctionScheduler.js
@@ -0,0 +1,14 @@
+getJasmineRequireObj().DelayedFunctionScheduler = function(j$) {
+  function DelayedFunctionScheduler() {
+    const self = this
+    let scheduledLookup = []
+    let scheduledFunctions = {}
+    let currentTime = 0
+    let delayedFnCount = 0
+    let deletedKeys = []
+
+
+  }
+
+  return DelayedFunctionScheduler
+}
